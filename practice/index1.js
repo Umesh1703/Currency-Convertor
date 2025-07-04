a9ecd761231548e0744641c91928e39160bf075b
Umesh Chandra <pathipatiumesh@gmail.com>
2025-07-04 11:33:49 +0530

    Add currency convertor project

---
+++ practice/index1.js
@@ -0,0 +1,54 @@
+let baseURL = "https://api.frankfurter.app/latest";
+
+let dropdowns = document.querySelectorAll(".dropdown select");
+let btn = document.querySelector("form button");
+let fromCurr = document.querySelector(".from select");
+let toCurr = document.querySelector(".to select");
+let msg = document.querySelector(".message");
+
+
+for(let select of dropdowns){
+    for(let currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+
+        if(select.name === "from" && currCode === "USD"){
+            newOption.selected = "selected";
+        }
+        else if(select.name === "to" && currCode === "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change",() => {
+        updateFlag(select);
+    })
+}
+
+let updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click",async(evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1){
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${baseURL}?from=${fromCurr.value}&to=${toCurr.value}`;
+
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data.rates[toCurr.value];
+    let finalRate = amtVal*rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalRate} ${toCurr.value}`;
+})
